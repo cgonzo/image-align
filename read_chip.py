451c3570571f2db7474d476f6f7aac2ef184f56c
Chris Gonzales <cgonzo86@gmail.com>
2017-03-12 20:22:51 -0400

    additional config options

--- read_chip.py
+++ read_chip.py
@@ -26,15 +26,52 @@ ap.add_argument("-s", "--showalignment", help = "Display aligned images on scree
 ap.add_argument("-t", "--troubleshooting", help = "Troubleshooting mode", action="store_true")
 args = vars(ap.parse_args())
 
-# Read the images to be aligned
-im1 =  cv2.imread(args["image"])
+# Store arguments in pyton variables
 alignmentblur = args["alignmentblur"]
 dotsize = args["dotsize"]
 numdots = args["numdots"]
 output_file = open(args["output"],'w')
 config_file = open(args["config"],'r')
+im1 =  cv2.imread(args["image"])
 
-print "Image size is %d x %d" %(im1.shape[1],im1.shape[0])
+# Print info about the image we are given
+xsize = im1.shape[1]
+ysize = im1.shape[0]
+print "Image size is %d x %d" %(xsize,ysize)
+
+# Parse json config file
+config_contents = json.load(config_file)
+if args["troubleshooting"]:
+  pprint(config_contents)
+# Amount of padding from left when building reference image from reference dots
+padding=config_contents["padding"]
+# Scaling factor for reference dots. Right now, scaling X and Y by same factor
+scaling=config_contents["scaling"]
+# Get the locations of the dots we actually want to read. Note that these can be negative if we want to read to the left or top of a reference dot
+min_y=config_contents["min_y"]
+max_y=config_contents["max_y"]
+min_x=config_contents["min_x"]
+max_x=config_contents["max_x"]
+assert (min_x + padding) * scaling > 0 , "sample dots less than 0 once scaled in x direction. Increase padding or scaling"
+assert (min_y + padding) * scaling > 0 , "sample dots less than 0 once scaled in y direction. Increase padding or scaling"
+assert (max_x + padding) * scaling < xsize, "sample dots out of bounds once scaled in x direction. Reduce padding or scaling"
+assert (max_y + padding) * scaling < ysize, "sample dots out of bounds once scaled in y direction. Reduce padding or scaling"
+# Location of reference dots. [1,1] is top left corner
+ref_alignment_dots_raw = np.array(config_contents["ref_dots"])
+(ref_max_x,ref_max_y) = np.amax(ref_alignment_dots_raw,axis=0)
+(ref_min_x,ref_min_y) = np.amin(ref_alignment_dots_raw,axis=0)
+#  Make sure that scaling makes sense
+assert (ref_min_x + padding) * scaling > 0 , "reference dots less than 0 once scaled in x direction. Increase padding or scaling"
+assert (ref_min_y + padding) * scaling > 0 , "reference dots less than 0 once scaled in y direction. Increase padding or scaling"
+assert (ref_max_x + padding) * scaling < xsize, "reference dots out of bounds once scaled in x direction. Reduce padding or scaling"
+assert (ref_max_y + padding) * scaling < ysize, "reference dots out of bounds once scaled in y direction. Reduce padding or scaling"
+# First of all, the images are mirrored on the X-axis, so need to subtract X from ref_max_x
+ref_alignment_dots_mirrored = ref_alignment_dots_raw.copy()
+ref_alignment_dots_mirrored[:,0] = ref_max_x - ref_alignment_dots_raw[:,0]
+# Add padding to the left and top. Right now, we use the same padding for both
+ref_alignment_dots_padded = ref_alignment_dots_mirrored + padding
+# Now scale this to the size of the image
+ref_alignment_dots_scaled = ref_alignment_dots_padded * scaling
 
 # Convert image to grayscale
 im1_gray = cv2.cvtColor(im1,cv2.COLOR_BGR2GRAY)
@@ -59,55 +96,31 @@ im1_lower = im1_dots_sorted[0:6]
 im1_lower_sorted = im1_lower[im1_lower[:,0].argsort()]
 im1_dots_sorted = np.concatenate((im1_lower_sorted, im1_upper_sorted))
 
-# Right now, all the sample images are 1024x1280, so only support that resolution
-# Would be easy enough to scale either the images or sampling matrix to a new resolution
-# The chip is 17x18. 
-# From the layout.xls file:
-# Alignment dots in the matrix are in the first row at 1,4,7,10,13,18
-# In the 17th row at 1,4,7,14,16,18
-config_contents = json.load(config_file)
-pprint(config_contents["ref_dots"])
-ref_alignment_dots_18x17 = np.array(config_contents["ref_dots"])
-
-# First of all, the images are mirrored on the X-axis, so need to subtract X from 18
-ref_alignment_dots_mirrored = ref_alignment_dots_18x17.copy()
-ref_alignment_dots_mirrored[:,0] = 18 - ref_alignment_dots_18x17[:,0]
-# The chip is 17x18. To make this easier, we'll use center points in a 32x32 matrix
-# This means paddings of 7 on the left right and top, 8 on bottom
-ref_padding=7
-ref_alignment_dots_32x32 = ref_alignment_dots_mirrored + ref_padding
-
-#Now to scale this to a height of 1024, multiply by 32
-ref_scaling=32
-ref_alignment_dots_1280x1024 = ref_alignment_dots_32x32 * ref_scaling
-
-
 # Calculate homography using the image and alignment dots. 
 # We're using RANSAC since it seems to give the best answer
-warp_matrix, status = cv2.findHomography(im1_dots_sorted, ref_alignment_dots_1280x1024, cv2.RANSAC)
+warp_matrix, status = cv2.findHomography(im1_dots_sorted, ref_alignment_dots_scaled, cv2.RANSAC)
 percent_inliers = np.sum(status)/numdots
 print "Percentage of inliers: %f" %(percent_inliers*100)
 
 # Then line up im1 with alignment dots
-sz = im1.shape
-im1_aligned = cv2.warpPerspective (im1, warp_matrix, (sz[1],sz[0]))
+im1_aligned = cv2.warpPerspective (im1, warp_matrix, (xsize,ysize))
 im1_aligned_gray = cv2.cvtColor(im1_aligned,cv2.COLOR_BGR2GRAY)
 
 # Now measure all the points we care about
 dots = im1_aligned.copy()
-brightness = np.zeros((20,10))
+brightness = np.zeros((max_x-min_x,max_y-min_y))
 # We actually use the areas to the left and right of the array as references, so x is -1..19 rather than 1..18
 # for y it's even stranger; we only care about rows 7-16 (not the top 5 rows)
 print >>output_file, "Row,Column,Brightness"
-for y in range(7,17):
-  measurement_point_y = (y + ref_padding) * ref_scaling
-  for x in range(-1,19):
-    measurement_point_x = (x + ref_padding) * ref_scaling
+for y in range(min_y,max_y):
+  measurement_point_y = (y + padding) * scaling
+  for x in range(min_x,max_x):
+    measurement_point_x = (x + padding) * scaling
     for x_i in range(measurement_point_x - dotsize,measurement_point_x + dotsize):
       for y_i in range(measurement_point_y - dotsize,measurement_point_y + dotsize):
-        brightness[x+1,y-7] += im1_aligned_gray[y_i,x_i]
-    print >>output_file, "%d,%d,%d" %(y-7, x+1, brightness[x+1,y-7])
-    cv2.circle(dots, (measurement_point_x,measurement_point_y), dotsize*2, brightness[x+1,y-7]/10, -1)
+        brightness[x-min_x,y-min_y] += im1_aligned_gray[y_i,x_i]
+    print >>output_file, "%d,%d,%d" %(y-min_y, x-min_x, brightness[x-min_x,y-min_y])
+    cv2.circle(dots, (measurement_point_x,measurement_point_y), dotsize*2, brightness[x-min_x,y-min_y]/10, -1)
 cv2.imwrite("dots.png",dots)
 cv2.imshow("dots",dots)
 cv2.waitKey(0)
