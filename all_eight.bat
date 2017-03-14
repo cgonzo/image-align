b1107e3a76ce4dd89be3b2bb3894191d416cb3f2
Sarah Stidham <sarah@pineisland.me>
2017-03-14 19:23:44 -0400

    flip slide orientation and opening all images simultaneously script

---
+++ all_eight.bat
@@ -0,0 +1,8 @@
+python read_chip.py -i "170310\Sarah Sausage pH 8.4\0ppm R1 Sausage.png" -o "170310\Sarah Sausage pH 8.4\0ppm R1.csv" -c slide.json -m -a31
+python read_chip.py -i "170310\Sarah Sausage pH 8.4\0ppm R2 Sausage.png" -o "170310\Sarah Sausage pH 8.4\0ppm R2.csv" -c slide.json -m -a17
+python read_chip.py -i "170310\Sarah Sausage pH 8.4\0ppm R3 Sausage.png" -o "170310\Sarah Sausage pH 8.4\0ppm R3.csv" -c slide.json -m -a17
+python read_chip.py -i "170310\Sarah Sausage pH 8.4\0ppm R4 Sausage.png" -o "170310\Sarah Sausage pH 8.4\0ppm R4.csv" -c slide.json -m -a17
+python read_chip.py -i "170310\Sarah Sausage pH 8.4\50ppm R1 Sausage.png" -o "170310\Sarah Sausage pH 8.4\50ppm R1.csv" -c slide.json -m -a17
+python read_chip.py -i "170310\Sarah Sausage pH 8.4\50ppm R2 Sausage.png" -o "170310\Sarah Sausage pH 8.4\50ppm R2.csv" -c slide.json -m -a17
+python read_chip.py -i "170310\Sarah Sausage pH 8.4\50ppm R3 Sausage.png" -o "170310\Sarah Sausage pH 8.4\50ppm R3.csv" -c slide.json -m -a17
+python read_chip.py -i "170310\Sarah Sausage pH 8.4\50ppm R4 Sausage.png" -o "170310\Sarah Sausage pH 8.4\50ppm R4.csv" -c slide.json -m -a17
