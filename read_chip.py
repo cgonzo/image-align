59c3bb3e2059d2e5e9c125ce4d81406b33114f1b
Chris Gonzales <cgonzo86@gmail.com>
2017-03-14 15:15:30 -0400

    stop unnecessary debug print statements

--- read_chip.py
+++ read_chip.py
@@ -67,7 +67,6 @@ if args["troubleshooting"]:
 # Now we need to sort the reference dots. We want to sort by y descending and then x ascending
 ref_alignment_dots_order = np.lexsort((-ref_alignment_dots_raw[:,0],ref_alignment_dots_raw[:,1]))
 ref_alignment_dots_sorted = ref_alignment_dots_raw[ref_alignment_dots_order]
-pprint(ref_alignment_dots_sorted)
 #  Make sure that scaling makes sense
 assert (ref_min_x + padding) * scaling > 0 , "reference dots less than 0 once scaled in x direction. Increase padding or scaling"
 assert (ref_min_y + padding) * scaling > 0 , "reference dots less than 0 once scaled in y direction. Increase padding or scaling"
@@ -99,15 +98,12 @@ for i in xrange(numdots):
 #  to be in each row. We bin them into these rows and then sort by x.
 im1_dots_sorted_by_y = im1_dots[im1_dots[:,1].argsort()]
 ref_alignment_dots_bins,ref_alignment_dots_bins_counts = np.unique(ref_alignment_dots_scaled[:,1],return_counts=True)
-pprint(ref_alignment_dots_bins_counts)
 lower_limit=0
 im1_dots_sorted=np.empty(shape=[0,2])
 for i in range(0,ref_alignment_dots_bins_counts.size):
-  print i
   upper_limit = lower_limit + ref_alignment_dots_bins_counts[i]
   im1_subarray = im1_dots_sorted_by_y[lower_limit:upper_limit]
   im1_subarray_sorted = im1_subarray[im1_subarray[:,0].argsort()]
-  pprint(im1_subarray_sorted)
   im1_dots_sorted = np.concatenate((im1_dots_sorted, im1_subarray_sorted))
   lower_limit = upper_limit
 if args["troubleshooting"]:
